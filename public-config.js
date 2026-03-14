3d435796e89d46877037068be56417776145ba09
zerokky11 <kkykiki89@nate.com>
2026-03-14 21:46:51 +0900

    add

---
+++ public-config.js
@@ -0,0 +1,16 @@
+window.DASHBOARD_CONFIG = {
+  supabaseUrl: "https://lnbuklfqchzfrmlowadr.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuYnVrbGZxY2h6ZnJtbG93YWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NTQ5OTksImV4cCI6MjA4OTAzMDk5OX0.XGAWqe_jVzdMkVfJuNQv2lQ82Amt_dcHSRynIuP8wPY",
+  appStatusKey: "main",
+  historyLimit: 80,
+  tables: {
+    buySignals: "buy_signals",
+    sellSignals: "sell_signals",
+    risingSectorRows: "rising_sector_rows",
+    tradeHistory: "trade_history",
+    appStatus: "app_status",
+    signalSettings: "signal_settings",
+    signalPresets: "signal_presets",
+    lastMarket: "rising_stocks_last_market",
+  },
+};
